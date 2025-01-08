4be2881aa16f3ef4bf649a0aa3be14097ba5446a
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-01-08 18:47:02 +0900

    ✨ 출금 기능 추가
    사용자가 출금 금액을 입력하고 , 잔액에서 차감되는 기능 추가 , 작액 금액을 출금하려고 시도하면 잔액 만큼만 출금

--- atm.py
+++ atm.py
@@ -1,6 +1,4 @@
 
-# while 문을 이용해서 입금 출금 영수증 보기 종료라는 버튼을 누르기 전까지 계속해서 
-
 receipts = []
 balance = 3000 #기본 금액
 
@@ -9,8 +7,17 @@ while True:
         if num == '4':
             break
         if num == "1":
-                deposit_amount = int(input("입금액:"))
-                balance += deposit_amount
-                receipts.append(("입금",deposit_amount,balance))
-                print(f"입금하신 금액은 {deposit_amount} 현재잔액은 {balance}원 입니다")
+            deposit_amount = int(input("입금액:"))
+            balance += deposit_amount
+            receipts.append(("입금",deposit_amount,balance))
+            print(f"입금하신 금액은 {deposit_amount} 현재잔액은 {balance}원 입니다")
+        if num == "2":
+            withdraw_amount = int(input("출금할 금액을 입력하세요: "))
+        if  balance >= withdraw_amount:
+            balance -= withdraw_amount
+            receipts.append((-withdraw_amount, balance))
+            print(f"{withdraw_amount}원이 출급 되었습니다.")
+        else:
+            print("잔액이 부족합니다")
+
         print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
