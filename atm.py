101b307737caf92a38a1504ac68cfd4470a072c4
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-01-09 17:08:00 +0900

    🐛 입력 검증 및 에러 처리 추가
    잘못 입력된 값을 처리하도록 기능 추가, 유효하지 않은 메뉴 선택시 경고 메시지 출력

--- atm.py
+++ atm.py
@@ -1,3 +1,7 @@
+#입력 검증 및 에러 처리 추가
+#잘못된 입력 값(숫자가 아닌값, 음수 값 등)을 처리하도록 기능 추가
+#유효하지 않은 메뉴 선택 시 경고 메시지 또는 사용방범 재안내를 해주세요
+
 
 receipts = []
 balance = 3000 #기본 금액
@@ -7,17 +11,18 @@ while True:
         if num == '4':
             break
         if num == "1":
-            deposit_amount = int(input("입금액:"))
-            balance += deposit_amount
-            receipts.append(("입금",deposit_amount,balance))
-            print(f"입금하신 금액은 {deposit_amount} 현재잔액은 {balance}원 입니다")
+            deposit_amount = input("입금액:")
+            if deposit_amount.isdigit() and int(deposit_amount) > 0:
+                balance += int(deposit_amount)
+                receipts.append(("입금",deposi1t_amount,balance))
+                print(f"입금하신 금액은 {deposit_amount} 현재잔액은 {balance}원 입니다")
+            else:
+                print("입금한 금액을 숫자 형태와 음수가 아닌값을 입력해주세요")
         if num == "2":
             withdraw_amount = int(input("출금할 금액을 입력하세요: "))
-        if  balance >= withdraw_amount:
-            balance -= withdraw_amount
-            receipts.append((-withdraw_amount, balance))
-            print(f"{withdraw_amount}원이 출급 되었습니다.")
-        else:
-            print("잔액이 부족합니다")
+            if  balance >= withdraw_amount:
+                balance -= withdraw_amount
+                receipts.append((-withdraw_amount, balance))
+                print(f"{withdraw_amount}원이 출급 되었습니다.")   
 
-        print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
+            print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
